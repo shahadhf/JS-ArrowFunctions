73fd589c012c388af1b6b72e18262bb68922867e
shahadhf <salmarag@outlook.com>
2024-11-05 15:07:31 +0300

    done

---
+++ arrowFunction.js
@@ -0,0 +1,19 @@
+// Question 1
+
+const greet = (name) => `Hello, ${name}!`;
+
+// Question 2
+const add = (a, b) => {
+  return a + b;
+};
+
+//Question 3
+const square = (num) => {
+  return num * num;
+};
+
+//Challenge
+
+const squares = (numbers) => {
+  return numbers.map(numbers * numbers);
+};
